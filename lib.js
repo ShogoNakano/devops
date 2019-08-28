595044e080d8ff283959e8b52ac3c794f6d81a64
S.Nakano <shougo.nakano@cydas.com>
2019-08-28 23:17:41 +0900

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,11 +1,11 @@
 function calc(height) {
-    return (height - 100) * 0.9;
+    return (height - 100) * 0.8;
   }
   
   // there is test script
   
   const t1 = calc(180);
   
-  if (t1 !== 70) {
+  if (t1 !== 64) {
     throw new Error('test failed');
   }
